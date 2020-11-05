83bfd0eaa07686dc2f7e272c72a7806e63d851cb
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2020-11-05 02:28:40 +0000

    Use hastebin instead of pastebin

---
+++ src/commands/useful/hastebin.ts
@@ -0,0 +1,44 @@
+import { Content, Message } from "@mrwhale-io/gamejolt";
+import axios from "axios";
+
+import { Command } from "../command";
+
+export default class extends Command {
+  constructor() {
+    super({
+      name: "hastebin",
+      description: "Upload text to hastebin.",
+      type: "useful",
+      usage: "<prefix>hastebin <paste>",
+    });
+  }
+
+  async action(message: Message, [text]: [string]) {
+    if (!text || text === "") {
+      return message.reply("Please provide text to upload.");
+    }
+
+    const url = "https://hasteb.in/documents";
+    const config = {
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    };
+
+    try {
+      const result = await axios.post(url, text, config);
+
+      if (!result.data.key) {
+        return message.reply("Could not upload text.");
+      }
+
+      const content = new Content();
+      const link = `https://hasteb.in/${result.data.key}`;
+      content.insertText(link, [content.autoLink(link, link)]);
+
+      return message.reply(content);
+    } catch {
+      return message.reply("Could not upload text.");
+    }
+  }
+}
