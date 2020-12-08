804397f7bf849de6eb2f76080769f3bd2803f961
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2020-12-08 21:15:50 +0000

    Fix broken link

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -63,7 +63,7 @@ function Home() {
                 "button button--outline button--secondary button--lg",
                 styles.getStarted
               )}
-              to={useBaseUrl("docs/")}
+              to={useBaseUrl("docs/introduction")}
             >
               Get Started
             </Link>
