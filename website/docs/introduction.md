b6613c9f78f609a9def63e1f324fb1fec48576b5
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2020-12-08 21:08:57 +0000

    Create docusaurus website

---
+++ website/docs/introduction.md
@@ -0,0 +1,49 @@
+---
+id: introduction
+title: Introduction
+sidebar_label: Introduction
+---
+
+Mr. Whale is an all-purpose chat bot loaded with dozens of awesome commands and features. Mr. Whale has been the source of entertainment in over 500+ group chats and counting.
+
+## Table of Contents
+- [Features](#features)
+- [Links](#links)
+- [Maintainers](#maintainers)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+Some basic features include:
+
+- ✉️ Support for commands in private and group chats.
+- 🤖 Have conversations with cleverbot.
+- 🏆 EXP system for levelling up in group chats.
+
+Mr. Whale has a lot of commands, with 5 main categories:
+
+- 👻 **Fun**: `ascii`, `choose`, `conchshell`, `ship`, `gameidea` and more!
+- 🎲 **Game**: `hangman` and `rockpaper`!
+- 🖨️ **Useful**: `calculator`, `translate`, `hastebin` and `wiki`!
+- 🔧 **Utility**: `help`, `info`, `langs`, `rank` and more!
+- 👑 **Owner**: `eval`, `reload` and `cleverbot`!
+
+## Links
+*   [Discord](https://discord.gg/wjBnkR4AUZ)
+*   [Github](https://github.com/mrwhale-io/mrwhale/)
+*   [Game Jolt](https://gamejolt.com/@mrwhale)
+
+## Maintainers
+
+[@ttbowen](https://github.com/ttbowen)
+
+## Contributing
+
+Feel free to dive in! [Open an issue](https://github.com/mrwhale-io/mrwhale/issues/new) or submit PRs.
+
+Standard Readme follows the [Contributor Covenant](http://contributor-covenant.org/version/1/3/0/) Code of Conduct.
+
+## License
+
+[MIT](https://github.com/mrwhale-io/mrwhale/blob/master/LICENSE) © Thomas Bowen
---
+++ website/docusaurus.config.js
@@ -0,0 +1,94 @@
+module.exports = {
+  title: "Introducing Mr. Whale",
+  tagline: "The best chat bot on Game Jolt!",
+  url: "https://mrwhale-io.github.io",
+  baseUrl: "/mrwhale/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "mrwhale-io",
+  projectName: "mrwhale",
+  themeConfig: {
+    navbar: {
+      title: "Mr. Whale",
+      items: [
+        {
+          to: "docs/commands",
+          activeBasePath: "commands",
+          label: "Commands",
+          position: "left",
+        },
+        {
+          href: "https://discord.com/invite/wjBnkR4AUZ",
+          label: "Discord Server",
+          position: "left",
+        },
+        {
+          href: "https://github.com/mrwhale-io/mrwhale",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Introduction",
+              to: "docs/introduction/",
+            },
+            {
+              label: "Commands",
+              to: "docs/commands/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discord.com/invite/wjBnkR4AUZ",
+            },
+            {
+              label: "Game Jolt",
+              href: "https://gamejolt.com/c/mrwhale-tifrgr",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/mrwhale-io/mrwhale",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Mr. Whale. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/mrwhale-io/mrwhale/edit/master/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            "https://github.com/mrwhale-io/mrwhale/edit/master/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
