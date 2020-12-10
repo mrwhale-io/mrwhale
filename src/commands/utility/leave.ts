8874d761f02c9fd162d3ea49943b7e58cb2aae5d
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2020-12-10 01:18:16 +0000

    Add leave command

---
+++ src/commands/utility/leave.ts
@@ -0,0 +1,25 @@
+import { Message } from "@mrwhale-io/gamejolt";
+
+import { Command } from "../command";
+
+export default class extends Command {
+  constructor() {
+    super({
+      name: "leave",
+      description: "Make me leave the group chat.",
+      type: "utility",
+      usage: "<prefix>leave",
+    });
+  }
+
+  async action(message: Message): Promise<void> {
+    const room = this.client.chat.activeRooms[message.room_id];
+
+    if (room && message.user.id === room.owner_id) {
+      message.reply("Goodbye 👋");
+      this.client.chat.userChannel?.push('group_leave', { room_id: room.id });
+    } else {
+      message.reply("You need to be room owner to use this command.");
+    }
+  }
+}
