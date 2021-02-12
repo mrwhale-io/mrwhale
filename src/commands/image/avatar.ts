f3cbf995790f8aa255f264c284a9847eccf2ef81
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2021-02-12 23:30:23 +0000

    Create avatar command

---
+++ src/commands/image/avatar.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { Content, Message } from "@mrwhale-io/gamejolt";
+
+import { Command } from "../command";
+
+export default class extends Command {
+  constructor() {
+    super({
+      name: "avatar",
+      description: "Display a user's avatar.",
+      type: "image",
+      usage: "<prefix>avatar <@username>",
+      cooldown: 5000,
+    });
+  }
+
+  async action(message: Message): Promise<Message> {
+    const content = new Content();
+    try {
+      let user = message.mentions[0];
+      if (!user) {
+        user = message.user;
+      }
+
+      const avatarFile = await axios.get(user.img_avatar, {
+        responseType: "stream",
+      });
+
+      const mediaItem = await this.client.chat.uploadFile(
+        avatarFile.data,
+        message.room_id
+      );
+
+      await content.insertImage(mediaItem);
+
+      return message.reply(content);
+    } catch {
+      return message.reply("Could not fetch avatar.");
+    }
+  }
+}
