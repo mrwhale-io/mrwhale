09ab4d59af97dd69a87f3a5d5caa9a5d115a7ec4
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2021-02-17 04:34:52 +0000

    Adjust pixel width of the username text

--- src/commands/utility/rank.ts
+++ src/commands/utility/rank.ts
@@ -28,8 +28,7 @@ const applyText = (canvas, text) => {
   do {
     // Assign the font to the context and decrement it so it can be measured again
     ctx.font = `${(fontSize -= 10)}px sans-serif`;
-    // Compare pixel width of the text to the canvas minus the approximate avatar size
-  } while (ctx.measureText(text).width > canvas.width - 300);
+  } while (ctx.measureText(text).width > canvas.width - 600);
 
   // Return the result to use in the actual canvas
   return ctx.font;
