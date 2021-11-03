5be0202b4e6af442867851f123d4adcff68a8bfd
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2021-11-03 23:57:42 +0000

    Fix the ship command

--- packages/mrwhale-discord/src/commands/fun/ship.ts
+++ packages/mrwhale-discord/src/commands/fun/ship.ts
@@ -7,17 +7,23 @@ export default class extends DiscordCommand {
   constructor() {
     super(ship.data);
     this.slashCommandData.addMentionableOption((option) =>
-      option.setName("first").setDescription("The first user.").setRequired(true)
+      option
+        .setName("first")
+        .setDescription("The first user.")
+        .setRequired(true)
     );
     this.slashCommandData.addMentionableOption((option) =>
-      option.setName("second").setDescription("The second user.").setRequired(true)
+      option
+        .setName("second")
+        .setDescription("The second user.")
+        .setRequired(true)
     );
   }
 
   async action(interaction: CommandInteraction): Promise<void> {
-    const first = interaction.options.getString("first");
-    const second = interaction.options.getString("second");
-    const result = ship.action(`<@${first}>`, `<@${second}>`);
+    const first = interaction.options.getMentionable("first").toString();
+    const second = interaction.options.getMentionable("second").toString();
+    const result = ship.action(first, second);
 
     return interaction.reply(result);
   }
