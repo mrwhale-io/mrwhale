5792e5911dedf028ec76b6f826a93d3d8f77ae29
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2021-11-28 20:38:23 +0000

    Load guild settings on ready event

--- packages/mrwhale-discord/src/client/discord-bot-client.ts
+++ packages/mrwhale-discord/src/client/discord-bot-client.ts
@@ -31,8 +31,10 @@ export class DiscordBotClient extends BotClient<DiscordCommand> {
     this.commandLoader.loadCommands();
     this.commandDispatcher = new DiscordCommandDispatcher(this);
     this.guildStorageLoader = new GuildStorageLoader(this);
-    this.guildStorageLoader.init();
-    this.guildStorageLoader.loadStorages();
+    this.client.on("ready", () => {
+      this.guildStorageLoader.init();
+      this.guildStorageLoader.loadStorages();
+    });
   }
 
   /**
