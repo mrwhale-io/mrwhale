941c0e26c2e9f8578485ddd21c0d8248e7904b6d
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2023-08-28 14:51:56 +0100

    Add slash command for levelchannel

--- packages/mrwhale-discord/src/commands/utility/level-channel.ts
+++ packages/mrwhale-discord/src/commands/utility/level-channel.ts
@@ -1,4 +1,12 @@
-import { ChannelType, Message } from "discord.js";
+import {
+  APIInteractionDataResolvedChannel,
+  CacheType,
+  ChannelType,
+  ChatInputCommandInteraction,
+  InteractionResponse,
+  Message,
+  TextChannel,
+} from "discord.js";
 
 import { DiscordCommand } from "../../client/command/discord-command";
 
@@ -6,28 +14,56 @@ export default class extends DiscordCommand {
   constructor() {
     super({
       name: "levelchannel",
-      description: "Set the level up announcement channel.",
+      description: "Set the level up message channel.",
       type: "utility",
-      usage: "<prefix>level channel <channel>",
+      usage: "<prefix>levelchannel <channel>",
       guildOnly: true,
       callerPermissions: ["Administrator"],
     });
+    this.slashCommandData.addChannelOption((option) =>
+      option
+        .setName("channel")
+        .setDescription("The channel to put level up messages in.")
+        .setRequired(true)
+    );
   }
 
-  async action(message: Message): Promise<void | Message<boolean>> {
-    const settings = this.botClient.guildSettings.get(message.guildId);
+  async action(
+    message: Message
+  ): Promise<Message<boolean> | InteractionResponse<boolean>> {
     const channel = message.mentions.channels.first();
+
     if (!channel || channel.type !== ChannelType.GuildText) {
       return message.reply("You must pass a text based channel.");
     }
 
+    return this.setLevelChannel(message, channel);
+  }
+
+  slashCommandAction(
+    interaction: ChatInputCommandInteraction<CacheType>
+  ): Promise<unknown> {
+    const channel = interaction.options.getChannel("channel");
+
+    if (channel.type !== ChannelType.GuildText) {
+      return interaction.reply("You must pass a text based channel.");
+    }
+
+    this.setLevelChannel(interaction, channel);
+  }
+
+  private async setLevelChannel(
+    interaction: Message | ChatInputCommandInteraction,
+    channel: TextChannel | APIInteractionDataResolvedChannel
+  ): Promise<Message<boolean> | InteractionResponse<boolean>> {
+    const settings = this.botClient.guildSettings.get(interaction.guildId);
     if (settings) {
       settings.set("levelChannel", channel.id);
-      return message.reply(
+      return interaction.reply(
         `Successfully set level up channel to <#${channel.id}>`
       );
     }
 
-    return message.reply(`Could not set the channel to <#${channel.id}`);
+    return interaction.reply(`Could not set the channel to <#${channel.id}`);
   }
 }
