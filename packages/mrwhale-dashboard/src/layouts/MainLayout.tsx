0a38e995498348f7f97d44873d18717ca05c5aba
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2023-09-26 00:06:28 +0100

    Put policy under information

--- packages/mrwhale-dashboard/src/layouts/MainLayout.tsx
+++ packages/mrwhale-dashboard/src/layouts/MainLayout.tsx
@@ -30,6 +30,7 @@ const footers = [
     description: [
       { title: "Commands", href: "/commands" },
       { title: "Dashboard", href: "/dashboard" },
+      { title: "Privacy Policy", href: "/privacy" },
     ],
   },
   {
@@ -37,10 +38,6 @@ const footers = [
     description: [
       { title: "GitHub", href: "https://github.com/mrwhale-io/mrwhale" },
     ],
-  },
-  {
-    title: "Legal",
-    description: [{ title: "Privacy Policy", href: "/privacy" }],
   },
 ];
 
@@ -68,7 +65,7 @@ const MainLayout = () => {
       >
         <Grid container spacing={4} justifyContent="space-evenly">
           {footers.map((footer) => (
-            <Grid item xs={6} sm={3} key={footer.title}>
+            <Grid item xs={4} sm={3} key={footer.title}>
               <Typography variant="h6" color="text.primary" gutterBottom>
                 {footer.title}
               </Typography>
