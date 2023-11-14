98589ce62b7e518bf745c1b4bd1c0ecc30bc2add
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2023-11-14 08:24:22 +0000

    Remove the MessageContent intent

--- packages/mrwhale-discord/src/constants.ts
+++ packages/mrwhale-discord/src/constants.ts
@@ -3,7 +3,6 @@ import { GatewayIntentBits } from "discord.js";
 export const EMBED_COLOR = "#71b8ce";
 export const INTENTS = [
   GatewayIntentBits.Guilds,
-  GatewayIntentBits.MessageContent,
   GatewayIntentBits.GuildMessages,
   GatewayIntentBits.GuildMembers,
   GatewayIntentBits.DirectMessages,
