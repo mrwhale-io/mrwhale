a243eba4bd35f46f2c21a0f7a4248e05e299bfb9
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2023-11-22 21:49:04 +0000

    Refactor rockpaper commands

--- packages/mrwhale-discord/src/commands/game/rockpaper.ts
+++ packages/mrwhale-discord/src/commands/game/rockpaper.ts
@@ -1,17 +1,15 @@
-import { ChatInputCommandInteraction, InteractionResponse, Message } from "discord.js";
+import {
+  ChatInputCommandInteraction,
+  InteractionResponse,
+  Message,
+} from "discord.js";
 
+import { rockpaper } from "@mrwhale-io/commands";
 import { DiscordCommand } from "../../client/command/discord-command";
 
 export default class extends DiscordCommand {
   constructor() {
-    super({
-      name: "rockpaper",
-      description: "Rock. Paper. Scissors.",
-      type: "game",
-      usage: "<prefix>rockpaper <rock|paper|scissors>",
-      examples: ["<prefix>rockpaper scissors"],
-      aliases: ["rps"],
-    });
+    super(rockpaper.data);
     this.slashCommandData.addStringOption((option) =>
       option
         .setName("choice")
@@ -25,29 +23,8 @@ export default class extends DiscordCommand {
     );
   }
 
-  private compare(first: string, second: string) {
-    if (first === second) {
-      return "It's a tie!";
-    } else if (first === "scissors") {
-      if (second === "paper") return "Scissors wins! :v:";
-      else return "Rock wins! :fist:";
-    } else if (first === "rock") {
-      if (second === "scissors") return "Rock wins! :fist:";
-      else return "Paper wins! :hand_splayed:";
-    } else if (first === "paper") {
-      if (second === "rock") return "Paper wins! :hand_splayed:";
-      else return "Scissors wins! :v:";
-    }
-  }
-
   async action(message: Message, [choice]: [string]): Promise<Message> {
-    if (!choice || choice === "") {
-      return message.reply("Please pass a choice.");
-    }
-
-    const userChoice = choice.trim().toLowerCase();
-
-    return message.reply(this.rockPaperScissors(userChoice));
+    return message.reply(rockpaper.action(choice));
   }
 
   slashCommandAction(
@@ -55,29 +32,6 @@ export default class extends DiscordCommand {
   ): Promise<InteractionResponse<boolean>> {
     const choice = interaction.options.getString("choice");
 
-    return interaction.reply(this.rockPaperScissors(choice));
-  }
-
-  private rockPaperScissors(choice: string): string {
-    const compChoice = Math.random();
-    let compChoiceStr = "";
-
-    const validChoices = /\b(rock|paper|scissors)\b/;
-
-    if (!choice.match(validChoices)) {
-      return "Please pass rock, paper, scissors.";
-    }
-
-    if (compChoice < 0.34) {
-      compChoiceStr = "Rock";
-    } else if (compChoice <= 0.67) {
-      compChoiceStr = "Paper";
-    } else {
-      compChoiceStr = "Scissors";
-    }
-
-    const result = this.compare(choice, compChoiceStr.toLowerCase());
-
-    return `${compChoiceStr}. ${result}`;
+    return interaction.reply(rockpaper.action(choice));
   }
 }
