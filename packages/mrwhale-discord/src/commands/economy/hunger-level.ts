168d62826c8635ff03e798a7a9c6b110c538a113
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2024-04-29 22:37:23 +0100

    Implement a health bar for the hunger level

--- packages/mrwhale-discord/src/commands/economy/hunger-level.ts
+++ packages/mrwhale-discord/src/commands/economy/hunger-level.ts
@@ -1,10 +1,12 @@
 import {
   ChatInputCommandInteraction,
+  EmbedBuilder,
   InteractionResponse,
   Message,
 } from "discord.js";
 
 import { DiscordCommand } from "../../client/command/discord-command";
+import { EMBED_COLOR } from "../../constants";
 
 export default class extends DiscordCommand {
   constructor() {
@@ -18,18 +20,44 @@ export default class extends DiscordCommand {
   }
 
   async action(message: Message): Promise<Message<boolean>> {
-    return message.reply(this.getHungerLevel(message.guildId));
+    const embed = this.getHungerLevelEmbed(message.guildId);
+    return message.reply({ embeds: [embed] });
   }
 
   async slashCommandAction(
     interaction: ChatInputCommandInteraction
   ): Promise<InteractionResponse<boolean>> {
-    return interaction.reply(this.getHungerLevel(interaction.guildId));
+    const embed = this.getHungerLevelEmbed(interaction.guildId);
+    return interaction.reply({ embeds: [embed] });
   }
 
-  private getHungerLevel(guildId: string) {
+  private getHungerLevelEmbed(guildId: string) {
     const hungerLevel = this.botClient.getGuildHungerLevel(guildId);
+    const currentProgress = Math.floor((hungerLevel / 100) * 100);
+    const embed = new EmbedBuilder()
+      .setColor(EMBED_COLOR)
+      .addFields([
+        {
+          name: "Hunger Level",
+          value: `${this.drawHealthBar(hungerLevel)} ${currentProgress}%`,
+        },
+      ])
+      .setColor(EMBED_COLOR)
+      .setTitle("My hunger level")
+      .setDescription("Here is my hunger level");
 
-    return `My hunger level is ${+hungerLevel.toFixed(2)}`;
+    return embed;
+  }
+
+  private drawHealthBar(hungerLevel: number) {
+    const progressLength = 20;
+    const currentProgress = Math.floor((hungerLevel / 100) * progressLength);
+    let progressBar = "";
+
+    for (let i = 0; i < progressLength; i++) {
+      progressBar += currentProgress < i ? "░" : "▓";
+    }
+
+    return progressBar;
   }
 }
