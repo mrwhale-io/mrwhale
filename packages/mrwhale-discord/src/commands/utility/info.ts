b2e2894e38faefe7ecab148ab51f387e16fb88d2
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2024-05-28 19:24:56 +0100

    Improve help information on info command

--- packages/mrwhale-discord/src/commands/utility/info.ts
+++ packages/mrwhale-discord/src/commands/utility/info.ts
@@ -11,6 +11,52 @@ import { EMBED_COLOR } from "../../constants";
 
 const FRACTIONAL_DIGITS = 2;
 const MEM_UNIT = 1024;
+const FEATURES = [
+  {
+    title: "**🎣 Fishing Game**",
+    subtitle:
+      "Dive into the exciting fishing game with the following commands:",
+    items: [
+      "`/catch`: Try your luck and catch a variety of fish. Some are common, while others are rare and valuable!",
+      "`/feed`: Feed your caught fish to me and increase my satiety to earn rewards like EXP and gems.",
+      "`/whalemood`: See my current mood and satiety level in the server.",
+    ],
+  },
+  {
+    title: "**🛠️ Utility Features**",
+    subtitle: "",
+    items: [
+      "`/help`: Explore all available commands and see how I can assist you.",
+      "`/announcementchannel`: Set the channel all my bot announcements will get sent to.",
+      "`/levelchannel`: Set an announcement channel for level ups.",
+    ],
+  },
+  {
+    title: "**🏅 Rank and Leaderboards**",
+    subtitle: "",
+    items: [
+      "`/rank`: View your personalized rank card showcasing your achievements and progress.",
+      "`/leaderboard`: Check out the top players in your server or globally.",
+    ],
+  },
+  {
+    title: "**🎉 Fun & Games**",
+    subtitle: "",
+    items: [
+      "`/hangman`: Play the classic word-guessing game, Hangman, with your friends.",
+      "`/shop`: Browse and buy various items to enhance your fishing experience, like fishing rods and bait.",
+    ],
+  },
+  {
+    title: "**🌐 Community & Support**",
+    subtitle: "",
+    items: [
+      "Join the [official Discord server](<<DISCORD_SERVER>>) to get support, share feedback, and stay updated with the latest features and events.",
+      "Visit the [website](https://www.mrwhale.io) for more information and documentation.",
+      "Check out the [source code](https://github.com/mrwhale-io/mrwhale) on GitHub.",
+    ],
+  },
+];
 
 export default class extends DiscordCommand {
   constructor() {
@@ -20,7 +66,7 @@ export default class extends DiscordCommand {
       type: "utility",
       usage: "<prefix>info",
       aliases: ["uptime", "stats", "version", "about"],
-      cooldown: 3000,
+      cooldown: 10000,
     });
   }
 
@@ -41,46 +87,81 @@ export default class extends DiscordCommand {
   ): Promise<InteractionResponse<boolean>> | Promise<Message<boolean>> {
     const avatar = this.botClient.client.user.displayAvatarURL();
     const memoryUsage = process.memoryUsage().heapUsed / MEM_UNIT / MEM_UNIT;
+    const totalUsers = this.botClient.client.guilds.cache.reduce(
+      (acc, guild) => acc + guild.memberCount,
+      0
+    );
+    const systemUptime = process.uptime() * 1000;
+    const botUptime = this.botClient.client.uptime;
+    const botLatency = Date.now() - message.createdTimestamp;
+    const apiLatency = Math.round(this.botClient.client.ws.ping);
+
+    let description = `Hello! I'm **${this.botClient.client.user.username}**, your friendly and versatile Discord bot, designed to enhance your server experience with a variety of fun and useful features.\n\n`;
+
+    for (const feature of FEATURES) {
+      description += `${feature.title}\n${
+        feature.subtitle ? `${feature.subtitle}\n` : ""
+      }${feature.items
+        .map(
+          (item) =>
+            `- ${item.replace(
+              "<<DISCORD_SERVER>>",
+              this.botClient.discordServer
+            )}\n`
+        )
+        .join("")}\n`;
+    }
 
     const embed = new EmbedBuilder()
       .addFields([
         {
-          name: "Official Discord server",
-          value: `[Join my Discord server!](${this.botClient.discordServer})`,
-        },
-        {
-          name: "Source code",
-          value: "https://github.com/mrwhale-io/mrwhale",
-        },
-        {
-          name: "Website",
-          value: "https://www.mrwhale.io",
-        },
-        {
-          name: "Version",
+          name: "🤖 Version",
           value: this.botClient.version,
+          inline: true,
         },
         {
-          name: "Servers",
+          name: "📊 Servers",
           value: `${this.botClient.client.guilds.cache.size}`,
+          inline: true,
         },
         {
-          name: "Loaded commands",
+          name: "👥 Users",
+          value: `${totalUsers}`,
+          inline: true,
+        },
+        {
+          name: "📜 Loaded Commands",
           value: `${this.botClient.commands.size}`,
+          inline: true,
         },
         {
-          name: "Memory usage",
+          name: "💾 Memory Usage",
           value: `${memoryUsage.toFixed(FRACTIONAL_DIGITS)} MB`,
+          inline: true,
         },
         {
-          name: "Bot uptime",
-          value: `${TimeUtilities.convertMs(this.botClient.client.uptime)}`,
+          name: "⏲️ Bot Uptime",
+          value: `${TimeUtilities.convertMs(botUptime)}`,
+          inline: true,
+        },
+        {
+          name: "🖥️ System Uptime",
+          value: `${TimeUtilities.convertMs(systemUptime)}`,
+          inline: true,
+        },
+        {
+          name: "⚡ Bot Latency",
+          value: `${botLatency} ms`,
+          inline: true,
+        },
+        {
+          name: "🌐 Discord API Latency",
+          value: `${apiLatency} ms`,
+          inline: true,
         },
       ])
       .setColor(EMBED_COLOR)
-      .setDescription(
-        `Hi I'm ${this.botClient.client.user.username} a general purpose discord bot. Use the \`help\` command to see my commands`
-      )
+      .setDescription(description)
       .setThumbnail(avatar)
       .setTitle(`About ${this.botClient.client.user.username}`);
 
