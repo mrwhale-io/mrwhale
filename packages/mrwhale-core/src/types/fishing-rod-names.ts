4c103860fb06288a2f70b20e74c3456c084769ef
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2024-06-23 14:12:29 +0100

    Add new fishing rods

--- packages/mrwhale-core/src/types/fishing-rod-names.ts
+++ packages/mrwhale-core/src/types/fishing-rod-names.ts
@@ -1,7 +1,11 @@
 export type FishingRodNames =
   | "Basic Fishing Rod"
+  | "Spincast Fishing Rod"
   | "Bamboo Fishing Rod"
   | "Fiberglass Fishing Rod"
   | "Telescopic Fishing Rod"
   | "Surf Fishing Rod"
-  | "Deep Sea Fishing Rod";
+  | "Deep Sea Fishing Rod"
+  | "Carbon Fiber Fishing Rod"
+  | "Graphite Fishing Rod"
+  | "Electric Fishing Rod";
